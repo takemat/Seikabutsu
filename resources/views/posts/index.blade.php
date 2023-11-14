5c8b1cbc2bc0c40e049e739497cade091618a6f0
takemat <takematsu329@gmail.com>
2023-11-14 21:10:56 +0900

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+        <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a> </h2>
+                    <p class='body'>{{ $post->body }}</p>
+                    <!-- Post.phpに作ったisLikedByメソッドをここで使用 -->
+                    @if (!$post->isLikedBy(Auth::user()))
+                        <span class="likes">
+                            <i class="fas fa-heart like-toggle" data-post-id="{{ $post->id }}"></i>
+                        <span class="like-counter">{{$post->likes_count}}</span>
+                        </span><!-- /.likes -->
+                    @else
+                        <span class="likes">
+                            <i class="fas fa-heart heart like-toggle liked" data-post-id="{{ $post->id }}"></i>
+                        <span class="like-counter">{{$post->likes_count}}</span>
+                        </span><!-- /.likes -->
+                    @endif
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                </div>
+            @endforeach
+            {{ Auth::user()->name }}
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+        <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
+ </x-app-layout>
